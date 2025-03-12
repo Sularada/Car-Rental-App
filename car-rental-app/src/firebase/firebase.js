031d23a3346efa601b63561b6395c2595abcf406
Sularada <111848753+Sularada@users.noreply.github.com>
2025-03-12 13:14:31 +0300

    setup

---
+++ car-rental-app/src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGq4z3ofkwY2IHDf4Mmzi3P03YiMC7M68",
+  authDomain: "car-app-d0bc3.firebaseapp.com",
+  projectId: "car-app-d0bc3",
+  storageBucket: "car-app-d0bc3.firebasestorage.app",
+  messagingSenderId: "377364852484",
+  appId: "1:377364852484:web:0bddbff329a610fb976a51",
+  measurementId: "G-BZTZRXXBMD",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
